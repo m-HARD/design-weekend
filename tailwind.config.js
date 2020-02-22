408190638eacb727b88dbbf5e08f57164025c9a6
Mhard musab <mumusab268000@gmail.com>
2020-02-22 11:06:34 +0200

    designWeeken

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  theme: {
+    extend: {
+      spacing: {
+        '1/2': '50%',
+        '1/3': '33.333333%',
+        '2/3': '66.666667%',
+        '1/4': '25%',
+        '3/4': '75%',
+      }
+    }
+  },
+  variants: {
+    backgroundColor:['responsive','hover','focus','active'],
+  },
+  plugins: []
+}
